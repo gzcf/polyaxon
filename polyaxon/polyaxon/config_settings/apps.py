d3d164da75f24878f423d6d42dae25f848d262ea
gzc <zhecongguo@linkdoc.com>
2018-05-18 20:30:01 +0800

    format code

--- polyaxon/polyaxon/config_settings/apps.py
+++ polyaxon/polyaxon/config_settings/apps.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
-from polyaxon.utils import config
-
 PROJECT_APPS = (
     'polyaxon',
     'libs.apps.LibsConfig',
