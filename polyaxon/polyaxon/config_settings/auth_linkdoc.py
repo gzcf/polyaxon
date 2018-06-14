179e8cd938b3c96930f4e4d6848e5d67364206db
gzc <zhecongguo@linkdoc.com>
2018-06-14 14:03:02 +0800

    Expose linkdoc auth config environment variables

--- polyaxon/polyaxon/config_settings/auth_linkdoc.py
+++ polyaxon/polyaxon/config_settings/auth_linkdoc.py
@@ -2,10 +2,11 @@
 """
 使用 LDAP 验证代理服务接入零氪内部的域账号体系
 """
+from polyaxon.utils import config
 from .auth import AUTHENTICATION_BACKENDS
 
-POLYAXON_AUTH_LINKDOC = True
+POLYAXON_AUTH_LINKDOC = config.get_boolean('POLYAXON_AUTH_LINKDOC', is_optional=True)
 
 if POLYAXON_AUTH_LINKDOC:
-    LDAP_AUTH_PROXY_ENDPOINT = 'http://172.16.0.128:8080'
+    LDAP_AUTH_PROXY_ENDPOINT = config.get_string('POLYAXON_AUTH_LINKDOC_PROXY').rstrip('/')
     AUTHENTICATION_BACKENDS = ['auth_linkdoc.backend.LinkdocDomainBackend'] + AUTHENTICATION_BACKENDS
