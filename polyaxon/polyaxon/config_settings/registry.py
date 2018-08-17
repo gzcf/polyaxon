eb2f294f7b8833755ba874729d94bfaaca11adaa
gzc <zhecongguo@linkdoc.com>
2018-08-17 16:23:05 +0800

    Use localhost nodeport for registry

--- polyaxon/polyaxon/config_settings/registry.py
+++ polyaxon/polyaxon/config_settings/registry.py
@@ -9,6 +9,8 @@ REGISTRY_USER = config.get_string('POLYAXON_REGISTRY_USER', is_optional=True)
 REGISTRY_PASSWORD = config.get_string('POLYAXON_REGISTRY_PASSWORD', is_optional=True)
 REGISTRY_HOST_NAME = config.get_string('POLYAXON_REGISTRY_HOST', is_optional=True)
 REGISTRY_PORT = config.get_string('POLYAXON_REGISTRY_PORT', is_optional=True)
+REGISTRY_NODE_PORT = config.get_string('POLYAXON_REGISTRY_NODE_PORT', is_optional=True)
+REGISTRY_HOST = '{}:{}'.format('127.0.0.1', REGISTRY_NODE_PORT)
 
 EXTERNAL_REGISTRIES = config.get_string('POLYAXON_EXTERNAL_REGISTRIES', is_optional=True)
 if EXTERNAL_REGISTRIES:
